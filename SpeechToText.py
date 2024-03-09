4c450339f4ecaa47c1ddf42a712b2e291efb33ad
Aqdhas Ali <aqdhasali468@gmail.com>
2024-03-09 12:41:05 +0530

    Coverted the python script into a web app

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,53 +1,87 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
 import speech_recognition as sr
-import pyttsx3
 
 
 #Initializing the speech recognizer
 r = sr.Recognizer()
 
 # Function to identify the audio 
-def record_text(): 
-    #Loop in case of errors - Ex: If audio is unable to understand
+def record_text(audio_file=None):
+    while True:
 
-    while(1):
         try:
-            #Getting the microphone as the source of input
-            with sr.Microphone() as source2:
-                #Preparing the recognizer to recieve input
-                r.adjust_for_ambient_noise(source2, duration=0.2)
+            if audio_file is None:
+                #Getting the microphone as the source of input
+                with sr.Microphone() as source2:
+                    #Preparing the recognizer to recieve input
+                    r.adjust_for_ambient_noise(source2, duration=0.2)
 
-                #Listen for the user's answers
-                audio2 = r.listen(source2)
+                    #Listen for the user's answers
+                    audio2 = r.listen(source2)
+            else:
+                #Connvert audio file in AudioData object
+                with sr.AudioFile(audio_file) as source:
+                    audio2 = r.record(source)
 
-                #Using google's speech recognition to recognise the audio
-                myText = r.recognize_google(audio2)
+            #Using google's speech recognition to recognise the audio
+            myText = r.recognize_google(audio2)
 
-                return myText
+            return myText  
 
         #These except blocks are used in cases when the audio cannot be understood
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
+            return ""
 
         except sr.UnknownValueError:
-            print("An Unknown Error Occured!")
-    return
-
-#Function to export the audio into text
-def output_text(text):
-    f = open("output.txt","a")
-    f.write(text)
-    f.write("\n")
-    f.close();
-    return
-
-def answer_text():
-    print("To Retireve the Answers From the Output.txt File")
+            print("An Unknown Error Occured!") 
+            return "" 
 
 
 
-#Functions are are called repetitively. Therefore, the audio can be converted to text in a loop
-while(1):
-    text = record_text()
-    output_text(text)
 
-    print("Wrote Text")
+#Route to handle Speech Recognition
+@app.route('/speechToText', methods=['POST'])
+def speech_to_Text():
+    try:
+        if 'audio_data' in request.files:
+            #Getting the audio files from request
+            audio_file = request.files['audio2']
+
+            #Processing the files and getting the recognized data
+                    
+            text = record_text(audio_file)
+            
+            #Write the text into a file
+            with open("C:/Users/aqdha/OneDrive/Desktop/Jupyter Projects/Answer Recognition/output.txt","a") as f:
+                print("Writing text to output.txt:", text)
+                f.write(text + "\n")
+                f.close()
+
+            return "Text Written Successfully"+ text
+        else:
+            #If Audio File is not provided use the microphone to record
+            text = record_text()
+            
+            #Write the text into a file
+            with open("C:/Users/aqdha/OneDrive/Desktop/Jupyter Projects/Answer Recognition/output.txt","a") as f:
+                print("Writing text to output.txt:", text)
+                f.write(text + "\n")
+                f.close()
+
+            return "Text Written Successfully" + text 
+           
+
+    
+    except Exception as e:
+        return str(e)
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
