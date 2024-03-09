6fb2942b533c16e58203770ce0885661bb95f1c6
DinushiWanniarachchi <dinushinwanniarachchi@gmail.com>
2024-03-09 10:59:24 +0530

    commit
    UI

---
+++ interview-website/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div id='main'>
+        <div className='pr-heading'>
+            <h2>ACE YOUR</h2>
+            <h1><span>INTERVIEW</span> WITH ACE</h1>
+            <p className='details'>Unlock your potential and master the art of interviewing with Ace</p>
+            <div className='header-btns'>
+                <a href='#' className='header-btn'>Start Quiz</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
