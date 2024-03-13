876a14062c6aa60b32955f50da622b183e6cceb2
Aqdhas Ali <aqdhasali468@gmail.com>
2024-03-14 00:10:46 +0530

    Made changes to improve speech to text conversion

--- SpeechToText.py
+++ SpeechToText.py
@@ -8,37 +8,36 @@ import speech_recognition as sr
 #Initializing the speech recognizer
 r = sr.Recognizer()
 
+
 # Function to identify the audio 
 def record_text(audio_file=None):
-    while True:
+    try:
+        if audio_file is None:
+            #Getting the microphone as the source of input
+            with sr.Microphone() as source2:
+                #Preparing the recognizer to recieve input
+                r.adjust_for_ambient_noise(source2, duration=0.2)
 
-        try:
-            if audio_file is None:
-                #Getting the microphone as the source of input
-                with sr.Microphone() as source2:
-                    #Preparing the recognizer to recieve input
-                    r.adjust_for_ambient_noise(source2, duration=0.2)
+                #Listen for the user's answers
+                audio2 = r.listen(source2, timeout=10)
+        else:
+            #Connvert audio file in AudioData object
+            with sr.AudioFile(audio_file) as source:
+                audio2 = r.record(source)
 
-                    #Listen for the user's answers
-                    audio2 = r.listen(source2)
-            else:
-                #Connvert audio file in AudioData object
-                with sr.AudioFile(audio_file) as source:
-                    audio2 = r.record(source)
+        #Using google's speech recognition to recognise the audio
+        myText = r.recognize_google(audio2)
 
-            #Using google's speech recognition to recognise the audio
-            myText = r.recognize_google(audio2)
+        return myText  
 
-            return myText  
+    #These except blocks are used in cases when the audio cannot be understood
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        return ""
 
-        #These except blocks are used in cases when the audio cannot be understood
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
-            return ""
-
-        except sr.UnknownValueError:
-            print("An Unknown Error Occured!") 
-            return "" 
+    except sr.UnknownValueError:
+        print("An Unknown Error Occured!") 
+        return "" 
 
 
 
@@ -80,7 +79,11 @@ def speech_to_Text():
         return str(e)
     
 
-
+#Route to handle exiting the loop
+def exit_loop():
+    global continue_loop
+    continue_loop = False #Setting the loop to false to 'Exit'
+    return "Loop Exiting"
 
 if __name__ == "__main__":
     app.run(debug=True)
