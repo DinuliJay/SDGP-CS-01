c9caf0092090491ee92f3239ceeb452d89210dd8
Udula Lokitha <136421349+udulalokita@users.noreply.github.com>
2024-03-19 11:10:10 +0530

    Adding Quiz Page to App.js

--- interview-website/src/App.js
+++ interview-website/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import Rout from './Components/Login/rout'
+import {BrowserRouter} from 'react-router-dom';
+import Form from './Components/Quiz';
+import Rout from './Components/Login/rout';
 import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 import Feature from './Components/Feature';
 import Contact from './Components/Contact';
 
+
 function App() {
   return (
+
     <div className="App">
       <Navbar/>
       <Header/>
@@ -16,8 +19,8 @@ function App() {
       <BrowserRouter>
       <Rout/>
       </BrowserRouter>
+      <Form />
+      
     </div>
   );
 }
-
-export default App;
