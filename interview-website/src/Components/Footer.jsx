b9768e7d61fe9f02ff33b404f68ec06798787d7b
Aqdhas Ali <aqdhasali468@gmail.com>
2024-03-22 14:29:44 +0530

    made changes

--- interview-website/src/Components/Footer.jsx
+++ interview-website/src/Components/Footer.jsx
@@ -1,62 +1,55 @@
-import React from 'react'
-
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-        <footer className="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-5">
-                        <h5><i className="fa fa-road"></i> ACME CO INC.</h5>
-                        <div className="row">
-                            <div className="col-6">
-                                <ul className="list-unstyled">
-                                    <li><a href="">Product</a></li>
-                                    <li><a href="">Benefits</a></li>
-                                    <li><a href="">Partners</a></li>
-                                    <li><a href="">Team</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-6">
-                                <ul className="list-unstyled">
-                                    <li><a href="">Documentation</a></li>
-                                    <li><a href="">Support</a></li>
-                                    <li><a href="">Legal Terms</a></li>
-                                    <li><a href="">About</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <ul className="nav">
-                            <li className="nav-item"><a href="" className="nav-link pl-0"><i className="fa fa-facebook fa-lg"></i></a></li>
-                            <li className="nav-item"><a href="" className="nav-link"><i className="fa fa-twitter fa-lg"></i></a></li>
-                            <li className="nav-item"><a href="" className="nav-link"><i className="fa fa-github fa-lg"></i></a></li>
-                            <li className="nav-item"><a href="" className="nav-link"><i className="fa fa-instagram fa-lg"></i></a></li>
-                        </ul>
-                        <br/>
-                    </div>
-                    <div className="col-md-2">
-                        <h5 className="text-md-right">Contact Us</h5>
-                        <hr/>
-                    </div>
-                    <div className="col-md-5">
-                        <form>
-                            <fieldset className="form-group">
-                                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <textarea className="form-control" id="exampleMessage" placeholder="Message"></textarea>
-                            </fieldset>
-                            <fieldset className="form-group text-xs-right">
-                                <button type="button" className="btn btn-secondary-outline btn-lg">Send</button>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
-  )
-}
+      <footer className="footer">
+        <div className="footer-container">
+            
+          <div className="about-us-cont">
+            <h2>About Us</h2>
+            <p>
+              We are a group computer science undergraduate at IIT. This project
+              is part of our software development group project module. This
+              project is developed to help job seekers to face interviews
+              confidently by allowing them to taken on mutliple mock interviews.
+            </p>
+          </div>
 
-export default Footer
+          <div className="contact-us-cont">
+            <h2>Contact Us</h2>
+            <p>
+              For any Queries and Improvements please feel free to drop in a
+              message.
+            </p>
+          </div>
+
+          <div className="site-map-cont">
+            <h2>Site-Map</h2>
+            <ul>
+              <li>Home Page</li>
+              <li>Login | SignUp</li>
+              <li>Features</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+
+          <div className="points-cont">
+            <h2>Points To consider before facing an Interview Sucessfully</h2>
+            <ul>
+              <li>Prepare</li>
+              <li>Make your CV stand out</li>
+              <li>Be Confident</li>
+              <li>Maintain Proper Posture</li>
+              <li>Dress Appropriate</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="copyright"> Ace Interview 2024 ©</div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
